5ec255a75885ca802be0f18dea09d5b4037113cf
Imran-Siddiquii <siddiquiimran132@gmail.com>
2024-05-07 23:27:47 +0530

    refactor: show chatbox if user is selected

--- frontend/src/containers/Home/Containers/ChatBox/index.tsx
+++ frontend/src/containers/Home/Containers/ChatBox/index.tsx
@@ -21,80 +21,90 @@ const ChatBox = () => {
   const handleBackClick = () => {
     dispatch(makeSelectedChatEmpty());
   };
+
+  const showSelectChatDetails=()=>{
+    console.log('eeeeeeeeeee');
+    
+  }
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <div
-        style={{
-          backgroundColor: '#f5f5f5',
-          padding: '8px',
-          borderBottom: '1px solid #ddd',
-        }}
-      >
-        <Box display={'flex'} alignItems={'center'}>
-          <Box onClick={handleBackClick}>
-            <IconButton
-              size="small"
-              color="inherit"
-              sx={{
-                color: '#ffff',
-                background: '#1976d2',
-                display: { xs: 'flex', sm: 'none' },
-                '&:hover': {
-                  color: '#1976d2',
-                  background: '#fff',
-                },
-              }}
-            >
-              <ArrowBackRounded />
+      {Object.keys(selectChat).length > 0 ? (
+        <>
+          {' '}
+          <div
+            style={{
+              backgroundColor: '#f5f5f5',
+              padding: '8px',
+              borderBottom: '1px solid #ddd',
+            }}
+          >
+            <Box display={'flex'} alignItems={'center'}>
+              <Box onClick={handleBackClick}>
+                <IconButton
+                  size="small"
+                  color="inherit"
+                  sx={{
+                    color: '#ffff',
+                    background: '#1976d2',
+                    display: { xs: 'flex', sm: 'none' },
+                    '&:hover': {
+                      color: '#1976d2',
+                      background: '#fff',
+                    },
+                  }}
+                >
+                  <ArrowBackRounded />
+                </IconButton>
+              </Box>
+              <Box pl={2} pr={2} fontWeight={500} fontSize="1.5rem">
+                {chat && Object.keys(chat).length > 0 && (
+                  <div>{chat?.users[1]?.name}</div>
+                )}
+              </Box>
+              <Box ml={'auto'} onClick={showSelectChatDetails}>
+                <IconButton
+                  size="small"
+                  color="inherit"
+                  sx={{
+                    color: '#ffff',
+                    background: '#1976d2',
+                    '&:hover': {
+                      color: '#1976d2',
+                      background: '#fff',
+                    },
+                  }}
+                >
+                  <VisibilityOutlined />
+                </IconButton>
+              </Box>
+            </Box>
+          </div>
+          <div style={{ flexGrow: 1, overflowY: 'auto', padding: '8px' }}>
+            <div style={{ marginBottom: '8px' }}> 'dff'</div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              padding: '8px',
+              borderTop: '1px solid #ddd',
+            }}
+          >
+            <TextField
+              variant="outlined"
+              placeholder="Type a message..."
+              fullWidth
+              sx={{ mr: 1 }}
+            />
+            <IconButton>
+              <InsertEmoticonIcon />
             </IconButton>
-          </Box>
-          <Box pl={2} pr={2} fontWeight={500} fontSize="1.5rem">
-            {chat && Object.keys(chat).length > 0 && (
-              <div>{chat?.users[1]?.name}</div>
-            )}
-          </Box>
-          <Box ml={'auto'}>
-            <IconButton
-              size="small"
-              color="inherit"
-              sx={{
-                color: '#ffff',
-                background: '#1976d2',
-                '&:hover': {
-                  color: '#1976d2',
-                  background: '#fff',
-                },
-              }}
-            >
-              <VisibilityOutlined />
+            <IconButton>
+              <SendIcon />
             </IconButton>
-          </Box>
-        </Box>
-      </div>
-      <div style={{ flexGrow: 1, overflowY: 'auto', padding: '8px' }}>
-        <div style={{ marginBottom: '8px' }}> 'dff'</div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: '8px',
-          borderTop: '1px solid #ddd',
-        }}
-      >
-        <TextField
-          variant="outlined"
-          placeholder="Type a message..."
-          fullWidth
-          sx={{ mr: 1 }}
-        />
-        <IconButton>
-          <InsertEmoticonIcon />
-        </IconButton>
-        <IconButton>
-          <SendIcon />
-        </IconButton>
-      </div>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
